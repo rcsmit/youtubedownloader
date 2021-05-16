f87a9e9abd707255394c33b55ad004d8cb850f25
Rene Smit <rcx.smit@gmail.com>
2021-05-16 22:56:11 +0200

    Update youtubedownloader.py

--- youtubedownloader.py
+++ youtubedownloader.py
@@ -5,25 +5,6 @@ Created on Fri Nov 29 16:46:42 2019
 
 @author: rcxsm
 
-WORKS AGAIN. INFO BELOW JUST LEFT THERE FOR FUTURE REFERENCE
-
-https://github.com/nficano/pytube/issues/591
-
--> install library
-pip uninstall pytube
-pip install pytube3
--> inside
-NOPE  C:/Users/rcxsm/AppData/Local/Programs/Python/Python38/Lib/site-packages/pytube/pip/pytube/extract.py
-C:/users/rcxsmi/Anaconda3/lib/site-packages/pytube/extract.py
-
--> find:
-parse_qs(formats[i]["cipher"]) for i, data in enumerate(formats)
--> change it to
-parse_qs(formats[i]["signatureCipher"]) for i, data in enumerate(formats)
-
-https://stackoverflow.com/questions/64492922/pytube-only-works-periodically-keyerror-assets
-
-https://stackoverflow.com/questions/63132116/too-many-values-to-unpack-in-pytube-module-problem
 """
 import os
 import os.path
@@ -37,16 +18,14 @@ from time import sleep
 
 
 
-FFMPEGLOC = 'C://Users/rcxsm/Documents/phyton_scripts/'
-
 def cleanup(test_string):
-    """[summary]
+    """Avoid forbidden characters in file names
 
     Args:
-        test_string ([type]): [description]
+        test_string (string): the string to test
 
     Returns:
-        [type]: [description]
+        result_string (string) : string without the bad characters
     """
     bad_chars = [":",';', ',', '/', ':', '!', "'","*","(",")"]
     result_string = ''.join(i for i in test_string if not i in bad_chars)
@@ -55,7 +34,8 @@ def cleanup(test_string):
 
 def download(durl):
 
-    """[summary]
+    """ Download a file
+
     """
     s1 = (int(time.time()))
      # Title and Time
@@ -78,6 +58,7 @@ def download(durl):
     print("Downloading took " + str(s2x) + " seconds ....")
 
     # Converting - Not needed anymore since MP3's are downloaded directly
+    # FFMPEGLOC = 'C://Users/rcxsm/Documents/phyton_scripts/'
     # print("Converting... {_filename}")
     # os.chdir(FFMPEGLOC)
     # subprocess.call(['ffmpeg', '-i', mp4, mp3])
@@ -89,22 +70,28 @@ def download(durl):
 def countdown(t):
     """ Creating a countdown timer to show the wait time
 
-    https://www.geeksforgeeks.org/how-to-create-a-countdown-timer-using-python/
+        https://www.geeksforgeeks.org/how-to-create-a-countdown-timer-using-python/
     Args:
         t (integer): Wait time in seconds
-    """    
+    """
     while t:
         mins, secs = divmod(t, 60)
         timer = '{:02d}:{:02d}'.format(mins, secs)
-        print(timer, end="\r")
+        print(f"Waiting : {timer}", end="\r")
         time.sleep(1)
         t -= 1
 
 
 
 def downloadplaylist(url):
-    #url = "https://www.youtube.com/watch?list=PLTo6svdhIL1cxS4ffGueFpVCF756ip-ab"
-    YOUTUBE_STREAM_AUDIO = '140' # modify the value to download a different stream / mime_type="audio/mp4" abr="128kbps" acodec="mp4a.40.2"
+    """Downlaods the sound of the videos in a playlist
+
+
+    Args:
+        url (string): the URL with the word list in it
+    """
+
+    YOUTUBE_STREAM_AUDIO = '140' # modify the value to download a different stream [ mime_type="audio/mp4" abr="128kbps" acodec="mp4a.40.2"]
     playlist = Playlist(url)
 
     # this fixes the empty playlist.videos list
@@ -115,31 +102,24 @@ def downloadplaylist(url):
         try:
             mp4 = video.title + ".mp4"
             if os.path.isfile(mp4):
-                print (f"{n}/{len(playlist.video_urls)} | File {video.title}.mp4 exists already. ")
-                no_wait = True
+                print (f"{n+1}/{len(playlist.video_urls)} | File {video.title}.mp4 exists already. ")
+                wait = False
             else:
-
-
-                print (f"Downloading {n}/{len(playlist.video_urls)} - {video.title}")
+                print (f"Downloading {n+1}/{len(playlist.video_urls)} - {video.title}")
                 audioStream = video.streams.get_by_itag(YOUTUBE_STREAM_AUDIO)
                 audioStream.download()
-                no_wait = False
-
-
-
+                wait = True
         except:
-            print (f"ERROR Downloading {n}/{len(playlist.video_urls)} ")
+            print (f"ERROR Downloading {n+1}/{len(playlist.video_urls)} ")
                     # some videos give 403 error
                     # too lazy to implement this (yet)
                     # https://github.com/pytube/pytube/issues/399
-        if no_wait:
-            pass
-        else:
+            wait = False
+        if wait:
             wait_time = randint(10,30)
-            print(f"Waiting {wait_time} seconds...")
             countdown(wait_time)
 def main():
-    url = input("URL: ")
+    url = input("URL to download: ")
 
     if "list" in url:
         downloadplaylist(url)
@@ -148,3 +128,27 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+"""
+
+WORKS AGAIN. INFO BELOW JUST LEFT THERE FOR FUTURE REFERENCE
+
+https://github.com/nficano/pytube/issues/591
+
+-> install library
+pip uninstall pytube
+pip install pytube3
+-> inside
+NOPE  C:/Users/rcxsm/AppData/Local/Programs/Python/Python38/Lib/site-packages/pytube/pip/pytube/extract.py
+C:/users/rcxsmi/Anaconda3/lib/site-packages/pytube/extract.py
+
+-> find:
+parse_qs(formats[i]["cipher"]) for i, data in enumerate(formats)
+-> change it to
+parse_qs(formats[i]["signatureCipher"]) for i, data in enumerate(formats)
+
+https://stackoverflow.com/questions/64492922/pytube-only-works-periodically-keyerror-assets
+
+https://stackoverflow.com/questions/63132116/too-many-values-to-unpack-in-pytube-module-problem
+
+"""
